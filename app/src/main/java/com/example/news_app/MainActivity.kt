917ce8dcaf179e9a1bdb99ca5b99e5089e80a1d1
admin <bjl0615@gmail.com>
2023-08-14 16:42:31 +0900

    News Detail Page UI

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.news_app
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
@@ -35,7 +36,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        newsAdapter = NewsAdapter()
+        newsAdapter = NewsAdapter {url ->
+            startActivity(
+                Intent(this, WebViewActivity::class.java).apply {
+                    putExtra("url" , url)
+                }
+            )
+        }
         val newsService = retrofit.create(NewsService::class.java)
 
         binding.newsRecyclerView.apply {
--- app/src/main/java/com/example/news_app/NewsAdapter.kt
+++ app/src/main/java/com/example/news_app/NewsAdapter.kt
@@ -8,11 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.news_app.databinding.ItemNewsBinding
 
-class NewsAdapter : ListAdapter<NewsModel , NewsAdapter.ViewHolder>(diffUtil){
+class NewsAdapter(private val onClick : (String) -> Unit) : ListAdapter<NewsModel , NewsAdapter.ViewHolder>(diffUtil){
 
     inner class ViewHolder(private val binding : ItemNewsBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item : NewsModel) {
             binding.titleTextViews.text = item.title
+            binding.root.setOnClickListener {
+                onClick(item.link)
+            }
 
             Glide.with(binding.thumbnailImageView)
                 .load(item.imageUrl)
